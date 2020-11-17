44b272f9e80aef0821fa5915c1c2456be306b0ce
Bluebasil <emoreaua@uwaterloo.ca>
2020-11-17 15:15:57 -0500

    inherit handler

--- src/main/kotlin/chess_ai/Handler.kt
+++ src/main/kotlin/chess_ai/Handler.kt
@@ -1,8 +1,13 @@
 package chess_ai
+import com.amazonaws.services.lambda.runtime.Context
+import com.amazonaws.services.lambda.runtime.RequestHandler
 
-class Handler {
-    fun handleRequest(input: String): String {
+class Handler : RequestHandler<Map<String, String>, String>{
+    override fun handleRequest(event: Map<String, String>?, context: Context?): String {
+        val logger = context!!.logger
         println("Hello World!")
+        logger.log("CONTEXT: $context")
+        println(event)
         main(arrayOf())
         return "Success"
     }
