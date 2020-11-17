8556e0f45f54040f974eaeec53baec01a53f44d2
Bluebasil <emoreaua@uwaterloo.ca>
2020-11-17 16:46:25 -0500

    logger issue

--- src/main/kotlin/chess_ai/Handler.kt
+++ src/main/kotlin/chess_ai/Handler.kt
@@ -4,9 +4,8 @@ import com.amazonaws.services.lambda.runtime.RequestHandler
 
 class Handler : RequestHandler<Map<String, String>, String>{
     override fun handleRequest(event: Map<String, String>?, context: Context?): String {
-        val logger = context!!.logger
         println("Hello World!")
-        logger.log("CONTEXT: $context")
+        println(context)
         println(event)
         main(arrayOf())
         return "Success"
