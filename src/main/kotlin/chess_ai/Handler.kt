fe4f6cce89b46d3c7191a85d3a7a1b874d5513a5
Bluebasil <emoreaua@uwaterloo.ca>
2020-11-17 20:37:48 -0500

    test api input type

--- src/main/kotlin/chess_ai/Handler.kt
+++ src/main/kotlin/chess_ai/Handler.kt
@@ -1,13 +1,27 @@
 package chess_ai
 import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.RequestHandler
+import com.beust.klaxon.JsonObject
 
-class Handler : RequestHandler<Map<String, String>, String>{
-    override fun handleRequest(event: Map<String, String>?, context: Context?): String {
+class Handler : RequestHandler<Any, String>{
+    override fun handleRequest(event: Any, context: Context?): String {
         println("Hello World!")
         println(context)
-        println(event)
+
         test(arrayOf())
+
+        if (event is String) {
+            println("#String")
+            println(event)
+        } else if (event is JsonObject) {
+            println("#json")
+            println(event)
+        } else {
+            println("#${event::class.simpleName}")
+            println("#${event::class.qualifiedName}")
+            println(event)
+        }
+
         return "Success"
     }
 }
